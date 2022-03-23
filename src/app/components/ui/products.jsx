b78989bc4e506de7ed1be6ba405be4216b852c5a
Yehor-F <argo.yegor@gmail.com>
2022-03-23 12:54:07 +0100

    filtration

--- src/app/components/ui/products.jsx
+++ src/app/components/ui/products.jsx
@@ -6,21 +6,70 @@ import LeftBar from '../common/leftBar'
 import ProductsTable from '../common/productsTable'
 const Products = () => {
   const [currentPage, setCurrentPage] = useState(1)
+  const [selectedBrand, setSelectedBrand] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState([])
   const { products } = useProducts()
   const productsArray = Object.keys(products).map((el) => {
     return { ...products[el] }
   })
-  const productsCount = productsArray.length
-  const pageSize = 6
+  const { categories, brands } = useProducts()
+  const categoryArray = Object.keys(categories).map((el) => {
+    return categories[el]
+  })
+  const brandsArray = Object.keys(brands).map((el) => {
+    return brands[el]
+  })
+  const handleSortByCategory = (value) => {
+    selectedCategory.includes(value)
+      ? setSelectedCategory((prevState) =>
+          prevState.filter((el) => el !== value)
+        )
+      : setSelectedCategory((prevState) => [...prevState, value])
+  }
+  const handleSortByBrand = (value) => {
+    selectedBrand.includes(value)
+      ? setSelectedBrand((prevState) => prevState.filter((el) => el !== value))
+      : setSelectedBrand((prevState) => [...prevState, value])
+  }
+  const filteredProducts =
+    selectedBrand.length > 0 && selectedCategory.length > 0
+      ? productsArray.filter((el) => {
+          if (
+            selectedBrand.includes(el.brand) &&
+            el.category.some((cat) => selectedCategory.includes(cat))
+          ) {
+            return el
+          }
+        })
+      : selectedBrand.length > 0
+      ? productsArray.filter((el) => {
+          if (selectedBrand.includes(el.brand)) {
+            return el
+          }
+        })
+      : selectedCategory.length > 0
+      ? productsArray.filter((el) => {
+          if (el.category.some((cat) => selectedCategory.includes(cat)))
+            return el
+        })
+      : productsArray
+
   const handlePageChange = (pageIndex) => {
     setCurrentPage(pageIndex)
   }
-  const productsCrop = paginate(productsArray, currentPage, pageSize)
+  const pageSize = 6
+  const productsCount = filteredProducts.length
+  const productsCrop = paginate(filteredProducts, currentPage, pageSize)
 
   return (
     products && (
       <div className='row'>
-        <LeftBar />
+        <LeftBar
+          handleSortByCategory={handleSortByCategory}
+          handleSortByBrand={handleSortByBrand}
+          categoryArray={categoryArray}
+          brandsArray={brandsArray}
+        />
         <ProductsTable products={productsCrop} />
         <Pagination
           itemsCount={productsCount}
